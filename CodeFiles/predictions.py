552c7e426cb1e10cc555317c4451dddb94b1110e
karthiksatyasai7 <karthiksatyasai7@gmail.com>
2024-02-20 12:06:31 +0530

    Files Modified Based on Api

--- CodeFiles/predictions.py
+++ CodeFiles/predictions.py
@@ -1,33 +1,46 @@
 
-import pickle
-from CodeFiles import link_tester
-import numpy as np
+import requests
 
 
 
 
-with open("Models/gradient_classifier_98_accuracy", "rb") as file:
-    gbc = pickle.load(file)
 
-
-# Loading the normalization object
 
 
 
 def predictor(url):
 
-    obj = link_tester.FeatureExtraction(url)
-    x = np.array(obj.getFeaturesList()).reshape(1,30) 
-    y_pred =gbc.predict(x)[0]
+    url = 'https://ebq1om82jg.execute-api.us-east-1.amazonaws.com/phishingStage/phishingResource?url=http.com/wach?v=FKSaZyX6mU4'
+
+# Send a GET request to the URL
+    response = requests.get(url)
+    print(response)
+
+# Check if the request was successful (status code 200)
+    if response.status_code == 200:
+    # Parse the JSON response
+        data = response.json()
+
+    # Access the 'output' key from the JSON response
+        output = data.get('output')
+
+    # Print the value of the 'output' key
+        print("Output:", output)
+
+        return int(output)
+    else:
+    # Print an error message if the request was not successful
+        print("Error:", response.status_code)
 
 
             
-    if y_pred==1:
-        print("The link is Good link")
-        return 0
+    # if y_pred==1:
+    #     print("The link is Good link")
+    #     return 0
         
-    else:
-        print("The link is phishing link")
-        return 1
+    # else:
+    #     print("The link is phishing link")
+    #     return 1
 
+# predictor("h")
 
